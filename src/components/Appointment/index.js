32785cbf1e1439542574931e601dd6def03136eb
quangtienftu49 <quangtienftu49@gmail.com>
2022-01-29 14:22:43 -0500

    fix bug cannot cancel a create form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -60,7 +60,7 @@ export default function Appointment(props) {
       {mode === CREATE && (
         <Form
           interviewers={props.interviewers}
-          onCancel={back}
+          onCancel={() => back()}
           onSave={save}
         />
       )}
